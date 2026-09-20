78fc57ac3cc75c623f8b3ce3a714ef556a749ecd
KirillChurin <kirillchurin07@mail.ru>
2026-09-20 14:45:30 +0300

    feat: modified handling with environmental variables

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+use_dotenv = os.getenv("USE_DOTENV", "0")
+
+if (use_dotenv == "1"):
+    load_dotenv()
 
 MAX_BOT_TOKEN = os.getenv("MAX_BOT_TOKEN")
 if not MAX_BOT_TOKEN:
-    raise ValueError("Не найден MAX_BOT_TOKEN в переменных окружения. Проверьте файл .env")
+    raise ValueError(f"Не найден MAX_BOT_TOKEN в переменных окружения. {"Проверьте файл .env" if use_dotenv == "1" else ""}")
