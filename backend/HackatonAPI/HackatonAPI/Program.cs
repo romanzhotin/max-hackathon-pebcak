7ea36bdb17cd63012c9832fc273b6c0230e5083f
KirillChurin <kirillchurin07@mail.ru>
2026-09-21 22:28:45 +0300

    style: just a commit

--- backend/HackatonAPI/HackatonAPI/Program.cs
+++ backend/HackatonAPI/HackatonAPI/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
 var app = builder.Build();
 
 app.MapOpenApi();
