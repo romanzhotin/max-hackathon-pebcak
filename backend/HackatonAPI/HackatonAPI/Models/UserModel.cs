70a068f6414bbd1a953b14a0d04a6e286e7b8a9c
KirillChurin <kirillchurin07@mail.ru>
2026-09-26 10:16:33 +0300

    feat: added users controller
    Added users controller wich can create, read and update users from database

---
+++ backend/HackatonAPI/HackatonAPI/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HackatonAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public long? MaxId { get; set; }
+        public string? City { get; set; }
+        public List<string>? Categories { get; set; }
+    }
+}
